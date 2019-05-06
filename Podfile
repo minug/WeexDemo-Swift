39da28458fc8e7f647b9662aa35287cc3c3b5b61
minug <1124571232@qq.com>
2019-05-06 15:32:44 +0800

    add proj files

---
+++ Podfile
@@ -0,0 +1,9 @@
+#source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '10.0'
+
+target 'WeexDemo-Swift' do
+#  pod 'lottie-ios'
+  pod 'BindingX'
+  pod 'WeexSDK',:modular_headers=>true
+end
---
+++ WeexDemo-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeexDemo-Swift
+//
+//  Created by minug on 2019/5/6.
+//  Copyright © 2019 minug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
