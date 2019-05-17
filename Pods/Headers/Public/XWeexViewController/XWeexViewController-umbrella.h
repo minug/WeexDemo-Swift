d9e45d6b045381742c4d319d19850bc67bd48ed1
xminug <1124571232@qq.com>
2019-05-17 14:42:53 +0800

    移动目录，将weex 的编译文件移入项目工程中

--- Pods/Headers/Public/XWeexViewController/XWeexViewController-umbrella.h
+++
@@ -1,1 +0,0 @@
-../../../Target Support Files/XWeexViewController/XWeexViewController-umbrella.h
--- localProject/XWeexViewController/Example/Pods/Target Support Files/XWeexViewController/XWeexViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_XWeexViewController : NSObject
-@end
-@implementation PodsDummy_XWeexViewController
-@end
